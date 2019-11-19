803291276b4fe8e09c296c5964b908d51cc4f44d
Gabriel Strauss <gstrauss@c3r3s6.wethinkcode.co.za>
2019-11-19 10:19:54 +0200

    password hashing

--- db/users.js
+++ db/users.js
@@ -5,7 +5,8 @@ var userSchema = new mongoose.Schema({ //This is where bugSchema is defined.
      name: String,
      surname: String,
      email: String,
-     password: String
+     password: String,
+     isverified: { type: Boolean, default: false}
 });
  
 module.exports.userSchema = userSchema; //Export bugSchema so that models.js can access it.
--- pages/create.js
+++ pages/create.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const app = express()
+const bcrypt = require('bcrypt');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 mongoose.connect(`mongodb+srv://gstrauss:qwerty0308@matcha-ch0yb.gcp.mongodb.net/test?retryWrites=true&w=majority`);
@@ -9,12 +10,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var Models = require("../db/models");
 
 router.post('/create', bodyParser.urlencoded(), function(req, res, next){
+   
    var _user = new Models.user ({
       name: req.body.name,
       surname: req.body.surname,
-      email: req.body.email,
-      password: req.body.password
+      email: req.body.email
    });
+
+   bcrypt.hash(req.body.password, 10, function(err, hash){
+      _user.password = hash;
+   });
+
+
 
    Models.user.findOne({ email: req.body.email }, function(err, user) {
       if(err) {
