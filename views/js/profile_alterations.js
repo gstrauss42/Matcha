5e156d7c1d28d547945b7dbd16f270da3eedef54
jadonhansen <jadongavhansen@gmail.com>
2019-11-28 17:04:57 +0200

    done edit bio and images on profile

--- views/js/profile_alterations.js
+++ views/js/profile_alterations.js
@@ -1,44 +1,25 @@
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	
-	// When a person wants to edit their bio and clicks the gear
+	// adding and removing overlays to gear
 	document.getElementById("alter-bio").addEventListener("mouseover", () => applyOverlay("alter-bio"));
 	document.getElementById("alter-bio").addEventListener("mouseout", () => removeOverlay("alter-bio"));
-	document.getElementById("alter-bio").addEventListener("click", function () {
-		alert('Altering bio in progress');
-	});
 
-	// The following is adding and removing overlays to images as well as upload(click) events
+	// adding and removing overlays to images
 	document.getElementById("main-profile-img").addEventListener("mouseover", () => applyOverlay("main-profile-img"));
 	document.getElementById("main-profile-img").addEventListener("mouseout", () => removeOverlay("main-profile-img"));
-	document.getElementById("main-profile-img").addEventListener("click", function () {
-		
-	});
 
 	document.getElementById("profile-img-two").addEventListener("mouseover", () => applyOverlay("profile-img-two"));
 	document.getElementById("profile-img-two").addEventListener("mouseout", () => removeOverlay("profile-img-two"));
-	document.getElementById("profile-img-two").addEventListener("click", function () {
-		
-	});
 
 	document.getElementById("profile-img-three").addEventListener("mouseover", () => applyOverlay("profile-img-three"));
 	document.getElementById("profile-img-three").addEventListener("mouseout", () => removeOverlay("profile-img-three"));
-	document.getElementById("profile-img-three").addEventListener("click", function () {
-		
-	});
 
 	document.getElementById("profile-img-four").addEventListener("mouseover", () => applyOverlay("profile-img-four"));
 	document.getElementById("profile-img-four").addEventListener("mouseout", () => removeOverlay("profile-img-four"));
-	document.getElementById("profile-img-four").addEventListener("click", function () {
-		
-	});
 
 	document.getElementById("profile-img-five").addEventListener("mouseover", () => applyOverlay("profile-img-five"));
 	document.getElementById("profile-img-five").addEventListener("mouseout", () => removeOverlay("profile-img-five"));
-	document.getElementById("profile-img-five").addEventListener("click", function () {
-		
-	});
-
 });
 
 function applyOverlay(id) {
