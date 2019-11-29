18448facf80939d8d1b109d78960fd65f5ff1990
jadonhansen <jadongavhansen@gmail.com>
2019-11-29 16:19:23 +0200

    done friend profile

--- views/js/profile_friend.js
+++ views/js/profile_friend.js
@@ -1,8 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-	// adding and removing overlays to gear (personal profile)
-	document.getElementById("options").addEventListener("mouseover", () => applyOverlay("options"));
-    document.getElementById("options").addEventListener("mouseout", () => removeOverlay("options"));
+	// adding and removing overlays to images (personal profile)
+	document.getElementById("main-profile-img").addEventListener("mouseover", () => applyOverlay("main-profile-img"));
+	document.getElementById("main-profile-img").addEventListener("mouseout", () => removeOverlay("main-profile-img"));
+
+	document.getElementById("two").addEventListener("mouseover", () => applyOverlay("two"));
+	document.getElementById("two").addEventListener("mouseout", () => removeOverlay("two"));
+
+	document.getElementById("three").addEventListener("mouseover", () => applyOverlay("three"));
+	document.getElementById("three").addEventListener("mouseout", () => removeOverlay("three"));
+
+	document.getElementById("four").addEventListener("mouseover", () => applyOverlay("four"));
+	document.getElementById("four").addEventListener("mouseout", () => removeOverlay("four"));
+
+	document.getElementById("five").addEventListener("mouseover", () => applyOverlay("five"));
+    document.getElementById("five").addEventListener("mouseout", () => removeOverlay("five"));
 });
 
 function applyOverlay(id) {
