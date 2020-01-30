98f572623a67b48054958dbd6bf0aeb601f8fd19
jadonhansen <jadongavhansen@gmail.com>
2020-01-30 11:53:32 +0200

    tag array fix

--- pages/upload_picture.js
+++ pages/upload_picture.js
@@ -8,7 +8,8 @@ fs = require('fs-extra')
 
 multer = require('multer')
 util = require('util')
-upload = multer({limits: {fileSize: 2000000 },dest:'/goinfre/jhansen/Documents/matcha/uploads'})  //jadons dir.
+// upload = multer({limits: {fileSize: 2000000 },dest:'/goinfre/jhansen/Documents/matcha/uploads'})  //jadons dir.
+upload = multer({limits: {fileSize: 2000000 },dest:'/Users/Jadon/Documents/WTC PROJECTS/Semester 2/matcha-collab/uploads'})  //jadons laptop dir.
 
 router.post('/', upload.single('picture'), bodyParser.urlencoded(), function (req, res)
 {
