d4925a3d3f54b7ab8211348b1a72012dceac5088
gstrauss42 <gstrauss@student.wethinkcode.co.za>
2020-01-31 17:07:18 +0200

    updated forgot password relinking

--- pages/user_confirm.js
+++ pages/user_confirm.js
@@ -4,11 +4,12 @@ var Models = require("../models/models");
 
 router.get('/', function(req, res){
    var check = req.originalUrl.substring(1);
+   // insert user check for forgot password link
    Models.user.findOneAndUpdate({ verif:check }, { $set : { isverified: "true"}}, function(err, doc) {
       if(doc && doc.email){
          return res.render("login", {url: check});
       }
-      return res.render("error")
+      return res.render("oops")
    });
 })
 
