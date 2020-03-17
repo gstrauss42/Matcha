e05134ed64fa0d417df82128a13a305dd625e5ed
gstrauss42 <gstrauss18@gmail.com>
2020-03-17 14:34:47 +0200

    fixed the 2 bugs and am correctly passing user messages and contacts page contacts

--- pages/contacts.js
+++ pages/contacts.js
@@ -8,17 +8,18 @@ router.get('/', function(req, res){
         res.render('oops');
     else
     {
-        models.user.find({"email": req.session.name}, function(err, doc){
-            console.log(doc.viewed + "\n\n" + doc.contacts + "\n\n\n" + doc + "\n\n\n"); // doc can be looged but and its elements are present but acessing those elements in doc returns undefined
+        models.user.findOne({"email": req.session.name}, function(err, doc){
+            // console.log(doc.viewed + "\n\n" + doc.contacts + "\n\n\n" + doc + "\n\n\n"); // doc can be looged but and its elements are present but acessing those elements in doc returns undefined
             models.user.find({"isverified":true},function(err,chatters){
-                var results;
-                var i;
+                var results = new Array;
+                var i=0;
                 chatters.forEach(element => {
                     if(doc.contacts)
                     {
                         doc.contacts.forEach(names => {
                             if(names == element.email)
                             {
+                                results[i] = new models.user;
                                 results[i] = element;
                                 i++;
                             }
@@ -26,7 +27,7 @@ router.get('/', function(req, res){
                     }
                 });
                 res.render("contacts", {"contacts":results});
-                // console.log("\n\n"+ results +"\n\n");                
+                console.log("\n\n"+ results +"\n\n");                
             });
         });
     }
