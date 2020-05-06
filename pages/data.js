859794e384a78cccbf16d555e0f999f956738984
gstrauss42 <gstrauss18@gmail.com>
2020-05-06 17:13:48 +0200

    changed data to post

--- pages/data.js
+++ pages/data.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var models = require("../models/models");
+const bodyParser = require('body-parser');
 var router = express.Router();
 
-router.post("/", (req, res) => {
-    models.messages.find({"from": req.session.name, "to": "jadongavhansen@gmail.com"}, function(err, sent){
-        models.messages.find({"to": req.session.name, "from": "jadongavhansen@gmail.com"}, function(err, received){
-          res.json[sent, received];
+router.post("/", bodyParser.urlencoded({extended: true}), (req, res) => {
+    models.messages.find({"from": req.session.name, "to": req.body.send}, function(err, sent){
+        models.messages.find({"to": req.session.name, "from": req.body.send}, function(err, received){
+            var thing = new Array;
+            thing.splice(0, 0, sent);
+            thing.splice(1,0, received);
+          res.json(thing);
         })
     })
 });
