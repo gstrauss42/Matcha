6c01f11713da9f1c98d4f7b2528b5d9cf022232e
jadonhansen <jadongavhansen@gmail.com>
2020-05-06 18:11:12 +0200

    ajax frontend post request on page load

--- views/js/load_messages.js
+++ views/js/load_messages.js
@@ -1,7 +1,10 @@
-setInterval(load, 3000);
+window.addEventListener('DOMContentLoaded', (event) => {
+	load();
+});
+
+setInterval(load, 4000);
 
 function load() {
-	console.log('loading...');
 	let from_messages = new Array;
 	let to_messages = new Array;
 	let from_arr = new Array;
@@ -14,14 +17,14 @@ function load() {
 		data: send,
 		success: function(data) {
 			console.log('getting stuff: ', data);
-			// from_messages.forEach(element => {
-			// 	from_arr += `<p class=\"mb-0\">${element.message}</p><p class=\"msgInfo\">from ${element.from} at ${element.time}</p>`;
-			// });
-			// $('#text-receive').html(from_arr);
-			// to_messages.forEach(element => {
-			// 	to_arr += `<p class=\"mb-0\">${element.message}</p><p class=\"msgInfo\">from ${element.from} at ${element.time}</p>`;
-			// });
-			// $('#text-send').html(to_arr);
+			data[1].forEach(element => {
+				from_arr += `<div id=\"text-receive\" class=\"text-left\"><p class=\"mb-0\">${element.message}</p><p class=\"msgInfo\">from ${element.from} at ${element.time}</p></div>`;
+			});
+			$('#text-r').html(from_arr);
+			data[0].forEach(element => {
+				to_arr += `<div id=\"text-send\" class=\"text-right\"><p class=\"mb-0\">${element.message}</p><p class=\"msgInfo\">from ${element.from} at ${element.time}</p></div>`;
+			});
+			$('#text-s').html(to_arr);
         },
 		error: function () {
 			alert('error loading!');
