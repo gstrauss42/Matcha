f5951d4126e0e72192750ab6c18f04c57bd1a387
gstrauss42 <gstrauss18@gmail.com>
2020-05-06 17:25:47 +0200

    helped fix code

--- pages/data.js
+++ pages/data.js
@@ -4,13 +4,11 @@ const bodyParser = require('body-parser');
 var router = express.Router();
 
 router.post("/", bodyParser.urlencoded({extended: true}), (req, res) => {
-    models.messages.find({"from": req.session.name, "to": req.body.send}, function(err, sent){
-        models.messages.find({"to": req.session.name, "from": req.body.send}, function(err, received){
+    models.messages.find({"from": req.session.name, "to": req.body.email}, function(err, sent){
+        models.messages.find({"to": req.session.name, "from": req.body.email}, function(err, received){
             var thing = new Array;
             thing.splice(0, 0, sent);
             thing.splice(1,0, received);
-            console.log('received: ', received);
-            console.log('sent', sent);
             res.json(thing);
         });
     });
