fcdccaa947d7f90373b4b3561eb338cb18c6ad9d
jadonhansen <jadongavhansen@gmail.com>
2020-05-12 14:30:47 +0200

    added functionality to notifications page

--- pages/live_notifications.js
+++ pages/live_notifications.js
@@ -3,16 +3,25 @@ var models = require("../models/models");
 var router = express.Router();
 
 router.get('/update_read', (req, res) => {
-    models.notifications.find({email:req.session.name}, function(err, count){
-        // make all notifs 'read' = true. save
-        console.log('updated all notifications to read');
-    });   
+    models.notifications.find({ email : req.session.name }, function (err, notifications) {
+        if (err) {
+            console.log('Error getting notifications');
+        } else {
+            notifications.forEach(element => {
+                if (element.read === false) {
+                    element.read = true;
+                    element.save(() => {});
+                }
+            });
+            console.log('updated all notifications to read');
+        }
+    });
 });
 
 router.get("/", (req, res) => {
     models.notifications.find({email:req.session.name}, function(err, count){
         res.json(count.length);
-    });   
+    });
 });
 
 router.post('/', (req, res) => {
--- views/js/load_notifPage.js
+++ views/js/load_notifPage.js
@@ -7,7 +7,7 @@ function updateRead() {
 		type: 'GET',
 		url: '/live_notifications/update_read',
 		success: function (data) {
-			fetch_notifs();
+			clearNotifs();
 		},
 		error: function () {
 			alert('Error updating read notifications!')
@@ -18,7 +18,7 @@ function updateRead() {
 function fetch_notifs() {
 	notifArr = new Array;
 	oldNotifArr = new Array;
-	
+
 	$.ajax({
 		type: 'POST',
 		url: '/live_notifications',
@@ -26,43 +26,49 @@ function fetch_notifs() {
 			// new notifications
 			if (data.new.length !== 0) {
 				data.new.slice().reverse().forEach(element => {
-					notifArr += `<div class=\"alert alert-info alert-dismissible fade show ml-4 mr-4 pb-0\" role="alert">
-								<strong class=\"mr-2\">${element.name}</strong><small>${element.time}</small>
-								<p class=\"mb-3 notifContent\">${element.content}</p>
+					notifArr += `<div class=\"alert alert-info alert-dismissible fade show pb-0\" role="alert">
+									<strong class=\"mr-2\">${element.name}</strong><small>${element.time}</small>
+									<p class=\"mb-3 notifContent\">${element.content}</p>
 								</div>`;
 				});
 				if (data.new.length == 1)
 				heading = `<h6>You have ${data.new.length} unread notification:</h6>
-							<button id="clear" class="badge badge-light pt-1 pb-1 mb-1" name="clear">Mark all as read</button>`;
+							<button id="clear" class="badge badge-light pt-1 pb-1 mb-1" name="clear">Mark all as read</button>
+							<hr>`;
 				else
 				heading = `<h6>You have ${data.new.length} unread notifications:</h6>
-							<button id="clear" class="badge badge-light pt-1 pb-1 mb-1" name="clear">Mark all as read</button>`;;
+							<button id="clear" class="badge badge-light pt-1 pb-1 mb-1" name="clear">Mark all as read</button>
+							<hr>`;
 				$('#notifHead').html(heading);
 				$('#notifBox').html(notifArr);
 				const btnClear = document.getElementById('clear');
 				btnClear.addEventListener('click', updateRead);
 			} else {
-				heading = `<h6>No new notifications</h6>`;
+				heading = `<h6>No new notifications :)</h6><hr>`;
 				$('#notifHead').html(heading);
 			}
 			// old notifications
 			if (data.old.length !== 0) {
+				if (data.new.length == 0) {
+					notifArr = '';
+					$('#notifBox').html(notifArr);
+				}
 				data.old.slice().reverse().forEach(element => {
-					oldNotifArr += `<div class=\"alert alert-info alert-dismissible fade show ml-4 mr-4 pb-0\" role="alert">
-									<strong class=\"mr-2\">${element.name}</strong><small>${element.time}</small>
-									<p class=\"mb-0 notifContent\">${element.content}</p>
-									<form action="notifications" method="post">
-										<input type="hidden" name="identifier" />
-										<button class=\"close\" type="submit" name="dismiss" data-dismiss="alert" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-									</form>
-								</div>`;
+					oldNotifArr += `<div class=\"alert alert-info alert-dismissible fade show pb-0\" role="alert">
+										<strong class=\"mr-2\">${element.name}</strong><small>${element.time}</small>
+										<p class=\"mb-0 notifContent\">${element.content}</p>
+										<form action="notifications" method="post">
+											<input type="hidden" name="identifier" />
+											<button class=\"close\" type="submit" name="dismiss" data-dismiss="alert" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											</button>
+										</form>
+									</div>`;
 				});
 				if (data.old.length == 1)
-					heading = `<h6>You have ${data.old.length} read notification:</h6>`;
+					heading = `<h6>You have ${data.old.length} read notification:</h6><hr>`;
 				else
-					heading = `<h6>You have ${data.old.length} read notifications:</h6>`;
+					heading = `<h6>You have ${data.old.length} read notifications:</h6><hr>`;
 				$('#oldNotifHead').html(heading);
 				$('#oldNotifBox').html(oldNotifArr);
 			} else {
